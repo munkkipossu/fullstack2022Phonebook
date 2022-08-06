d372105f70fc516445d7be3768eefa9c84b43792
Teemu <damunq@gmail.com>
2022-08-06 17:57:28 +0300

    add solution to 3.8

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (request, response) => {
     response.send(
