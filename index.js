db5f175588ffcb52dccb06ca1da15d819ef3fb68
Teemu <damunq@gmail.com>
2022-08-06 19:10:05 +0300

    getting heroku to work

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 persons = [
@@ -26,6 +28,7 @@ persons = [
 ]
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -89,7 +92,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
