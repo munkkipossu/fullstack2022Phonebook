eacd68e8239653c71e39f0bdb800f1dcbfd9b18e
Teemu <damunq@gmail.com>
2022-08-06 20:16:43 +0300

    uibuild

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fullstack2022Phonebook
 Exercises for problems in the fullstack course for Part3
+
+Heroku deployment found here: https://afternoon-sierra-18142.herokuapp.com/
--- index.js
+++ index.js
@@ -29,6 +29,7 @@ persons = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
